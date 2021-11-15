2ecc821017437a9af6f6e08964d3889e666b62ff
tokap0 <takachin5353@gmail.com>
2021-11-15 10:43:17 +0900

    fix code

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
+
 try {
     const codemod_file = core.getInput('codemod');
     const transform_files = core.getInput('transform');
     const codemod = String(codemod_file); 
     const transforms = String(transform_files);
-    await exec.exec('npx jscodeshift -t ' + codemod + ' ' + transforms);
+    exec.exec('npx jscodeshift -t ' + codemod + ' ' + transforms);
 
 } catch (error){
     core.setFailed(error.message);
