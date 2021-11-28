d2745950ab6bc046a8980e5751cdb12a16787652
tokap0 <takachin5353@gmail.com>
2021-11-28 22:52:03 +0900

    add function of change policy name

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-
+const fs = require("fs");
 
 try {
     const codemod_file = core.getInput('codemod');
     const transform_files = core.getInput('transform');
+    const policy_name = core.getInput('policyname');
+
+    const str_codemod = String(codemod_file);
+    let js_source = fs.readFileSync(str_codemod,"utf8");
+    var js_changed = js_source.replace('mypolicy', policy_name);
+    fs.writeFile(codemod_file, js_changed, (err) => {
+		if (err) {
+			console.log('Failed to add policy name "' + policy_name + '".' + err);
+            throw err;
+		}else{
+			console.log('Add policy name successful!');
+		}
+	});
+
+
     const codemod = String(codemod_file); 
     const transforms = String(transform_files);
     exec.exec('npx jscodeshift -t ' + codemod + ' ' + transforms);
